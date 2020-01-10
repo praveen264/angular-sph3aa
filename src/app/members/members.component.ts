dc59709415c30c2a6feddb66554098ebe3c17051
praveen264 <pstraveen264@gmail.com>
2020-01-10 21:18:58 +0530

    2 files modified, 2 added, 2 deleted

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit,Injector } from '@angular/core';
 import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable()
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
@@ -7,17 +11,22 @@ import {Router} from '@angular/router';
 })
 export class MembersComponent implements OnInit {
 
-  constructor(private router:Router) { }
-  
-  datas=[
-    {id:"QWERT",name:"W.e.trivedi"}
-  ];
+  constructor(private router:Router,private http:HttpClient) { }
+  getdatas:any;
+
 
   ngOnInit() {
       if(localStorage.getItem('username')==null)
       {
         this.router.navigate(['/login']);
       }
+      this.view();
   }
-  
+  view()
+  {
+    this.http.get('/api/db.json').subscribe(data=>{
+      this.getdatas=data;
+      console.log(this.getdatas);
+    });
+  }
 }
